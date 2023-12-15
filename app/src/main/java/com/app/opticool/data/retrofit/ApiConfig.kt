8682430db5de6ad9c3f9f4fec4e9e6bc84ab3d5f
Kipotzzz <124210031@student.upnyk.ac.id>
2023-12-15 17:25:45 +0700

    yg tadi salah commit hehe...

--- app/src/main/java/com/app/opticool/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/app/opticool/data/retrofit/ApiConfig.kt
@@ -1,6 +1,7 @@
 package com.app.opticool.data.retrofit
 
 import com.app.opticool.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,8 +14,16 @@ object ApiConfig {
         } else {
             HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
+        val authInterceptor = Interceptor {
+            val req = it.request()
+            val requestHeaders = req.newBuilder()
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZnVsbE5hbWUiOiJ0ZXMiLCJpYXQiOjE3MDI2MjMxNjUsImV4cCI6MTcwMjYyNjc2NX0.grZJFFTwXfa1GTmPfDOuDQ7fxhEnQ_y8O3kgUgekT4s")
+                .build()
+            it.proceed(requestHeaders)
+        }
         val client = OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
             .build()
         val retrofit = Retrofit.Builder()
             .baseUrl("https://public-dot-opticool.et.r.appspot.com/")
